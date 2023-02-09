5b09c01bccc3d29b87ff4cab3302cb4c95a1be40
RenatSir <r.sirazev@gmail.com>
2023-02-09 19:30:15 +0300

    Task_021

---
+++ Task_021/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void LineDist(int xa, int ya, int za, int xb, int yb, int zb) //квадрат(x2-x1)2+(y2-y1)2+(z2-z1)2
+{
+    Double Distance = Math.Sqrt((Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2)));
+    double result = Math.Round(Distance, 2); // округление вывел в отдельную строку для удобства чтения
+    Console.WriteLine($"A ({xa}, {ya}, {za}); B ({xb}, {yb}, {zb}) -> {result}");
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+// Ввод данных от пользователя
+int xa = GetNum("Введите координаты Xa - ");
+int ya = GetNum("Введите координаты Ya - ");
+int za = GetNum("Введите координаты Za - ");
+int xb = GetNum("Введите координаты Xb - ");
+int yb = GetNum("Введите координаты Yb - ");
+int zb = GetNum("Введите координаты Zb - ");
+
+// Решение задачи
+LineDist(xa, ya, za, xb, yb, zb);
---
+++ Task_023/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void NumberDegree(int num)
+{
+    int i = 1;
+    Console.Write($"{num} -> ");
+    while (i < num)
+    {
+        Console.Write(Math.Pow(i, 3) + ", ");
+        i ++;
+    }
+    Console.WriteLine(Math.Pow(num, 3));
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+// Ввод данных от пользователя
+int num = GetNum("Введите число N - ");
+
+// Вывод данных
+NumberDegree(num);
