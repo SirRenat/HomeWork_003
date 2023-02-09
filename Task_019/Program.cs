b1c8504ebc541e3f46eeee4b79317eb22a511e93
RenatSir <r.sirazev@gmail.com>
2023-02-09 22:50:14 +0300

    Task_019

--- Task_019/Program.cs
+++ Task_019/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+
+void CheckNumber(int num)
+{
+    if (!(num > 9999 && num < 100000))
+    {
+        Console.WriteLine("Вы ввели некорректное число");
+        return;
+    }
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+// Ввод данных
+int num = GetNum("Введите пятизначное число - ");
+CheckNumber(num);
+
+// Выво данных
+// Polindrome(num);
